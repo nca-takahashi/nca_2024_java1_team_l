457f07a3b69154ae88750ded2f6f8c72de426093
HideakiTAKAHASHI <takahashi-hideaki@dandelions.co.jp>
2024-12-04 17:24:19 +0900

    イメージをマウスで動かす

--- nca_2024_team_left/src/sample/Main.java
+++ nca_2024_team_left/src/sample/Main.java
@@ -5,10 +5,14 @@ public class Main {
 	public static void main(String[] args) {
 
 		Ex ex = new Ex();
-
+/* 
 		ex.no00();
 
 		System.out.println("問題1: ボタンをクリックすると「Hello, World!」と表示します。");
 		ex.no01();
+ */
+
+		ex.moveImage();
+
 	}
 }
