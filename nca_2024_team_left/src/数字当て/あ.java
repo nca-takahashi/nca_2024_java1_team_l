5712b3be7aca1877a991de07e99352af1a0be5b8
浦中　伶緒 <ncr2419si0002@edu.nca.ac.jp>
2024-12-11 17:16:42 +0900

    a

---
+++ nca_2024_team_left/src/数字当て/あ.java
@@ -0,0 +1,27 @@
+package 数字当て;
+
+import javax.swing.JFrame;
+
+public class あ {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		JFrame frame = new JFrame(" ");
+		System.out.println("");
+		あ.no1();
+	
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ nca_2024_team_left/src/数字当て/い.java
@@ -0,0 +1,9 @@
+package 数字当て;
+
+import java.awt.BorderLayout;
+
+public class い {
+	public void no01() {
+		frame.setLayout(new BorderLayout());
+	}
+}
