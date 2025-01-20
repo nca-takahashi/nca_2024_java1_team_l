4f9fc6f20697ccfbe2d0d1628300208a797c3938
BALTAZARAYLASKEIJIFRANCIS <ncr2419si0005@edu.nca.ac.jp>
2025-01-20 17:12:05 +0900

    音声

--- nca_2024_team_left/src/test/check.java
+++ nca_2024_team_left/src/test/check.java
@@ -1,46 +1,47 @@
 package test;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class check {
-	
-	public static boolean checkClick = false;
-	
+
     public static void main(String[] args) {
-        // フレームを作成
-        JFrame frame = new JFrame("Absolute Layout Example");
-        frame.setSize(800, 600);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(null);
-        
-        JButton button = new JButton();
-        button.setBounds(330,100,150,50);
-        button.setOpaque(false);
-        button.setContentAreaFilled(false);
-        button.setBorderPainted(false);
-        frame.add(button);
-        
-        System.out.println("checkClickは" + checkClick);
-        
-        button.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-        	// クリックイベントが発生した時に実行される処理
-        	checkClick = true;
-        	System.out.println("checkClickは" + checkClick);
-        	}
-        });
-        
-        JLabel up = new JLabel("この上");
-        up.setBounds(330,130,150,50);
-        frame.add(up);
-        
-        frame.setResizable(false);
-        frame.setVisible(true);
+        // 再生するWAVファイルのパスを指定
+        File soundFile = new File("C:\\Users\\cre\\git\\nca_2024_java1_team_l\\nca_2024_team_left\\bin\\test\\point.wav");
+
+        try {
+            // オーディオファイルを読み込む
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
+
+            // オーディオフォーマットとデータラインの取得
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioIn);
+
+            // 再生開始
+            clip.start();
+
+            // 再生が終了するまで待機
+            System.out.println("音声を再生中...");
+            while (!clip.isRunning()) {
+                Thread.sleep(100);
+            }
+            while (clip.isRunning()) {
+                Thread.sleep(100);
+            }
+
+            // 再生完了後にリソースを解放
+            clip.close();
+            audioIn.close();
+            System.out.println("再生完了");
+
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
